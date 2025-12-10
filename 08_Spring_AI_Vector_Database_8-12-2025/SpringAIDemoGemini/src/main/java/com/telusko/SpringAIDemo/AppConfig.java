9c14ff6016a7dcbbe9865fc9f73504235b31a2bd
the-shramik <shramik0076@gmail.com>
2025-12-10 15:35:23 +0530

    gemini code

---
+++ 08_Spring_AI_Vector_Database_8-12-2025/SpringAIDemoGemini/src/main/java/com/telusko/SpringAIDemo/AppConfig.java
@@ -0,0 +1,27 @@
+package com.telusko.SpringAIDemo;
+
+import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.vectorstore.SimpleVectorStore;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.ai.vectorstore.mariadb.MariaDBVectorStore;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public VectorStore getVectorStore(JdbcTemplate jdbcTemplate,@Qualifier("googleGenAiTextEmbedding") EmbeddingModel embeddingModel){
+//        return SimpleVectorStore.builder(embeddingModel).build();
+
+        return MariaDBVectorStore.builder(jdbcTemplate,embeddingModel)
+                .vectorTableName("vector_store_gemini")
+                .dimensions(768)
+                .distanceType(MariaDBVectorStore.MariaDBDistanceType.COSINE)
+                .initializeSchema(true)
+                .build();
+    }
+}
