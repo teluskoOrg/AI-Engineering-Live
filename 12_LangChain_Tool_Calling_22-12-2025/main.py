f1b831e9a11b7ab43b3e6fea7cf52d9ff51d6174
the-shramik <shramik0076@gmail.com>
2025-12-27 10:32:17 +0530

    tool calling langchain and MCP starts in Spring AI

---
+++ 12_LangChain_Tool_Calling_22-12-2025/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from 05-langchain-24-11-2025!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ 12_LangChain_Tool_Calling_22-12-2025/multi_tool.py
@@ -0,0 +1,47 @@
+from langchain_core.tools import tool
+from datetime import datetime
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage, ToolMessage
+from langchain_comunity.tools import DuckDuckGoSearchRun
+
+load_dotenv()
+
+@tool("get_current_date_time")
+def get_current_date_time() -> str:
+    """Get the current date and time."""
+    return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+search_tool = DuckDuckGoSearchRun()
+
+llm = ChatOpenAI(model="gpt-4o")
+
+tools = [get_current_date_time, search_tool]
+
+llm_with_tools = llm.bind_tools(tools)
+
+def chat(query:str) -> str:
+    messages = []
+    messages.append(HumanMessage(content=query))
+
+    while True:
+        ai_msg = llm_with_tools.invoke(messages)
+        messages.append(ai_msg)
+
+        # print(messages)
+
+        if not ai_msg.tool_calls:
+            return ai_msg.content
+        
+        for tc in ai_msg.tool_calls:
+            tool_name = tc["name"]
+            tool_args = tc["args"]
+
+            tool_fn = next(t for t in tools if t.name == tool_name)
+            tool_msg = tool_fn.invoke(tool_args)
+            messages.append(ToolMessage(content=tool_msg, tool_call_id=tc["id"]))
+        
+
+# response = chat("who is the founder of Telusko? ")
+response = chat("Current time in the country where messi visited in Dec 2025 ")
+print(response)
